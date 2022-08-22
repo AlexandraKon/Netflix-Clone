acb0fb323766fdaf381ed19740188abebfdc78cb
Alexandra Kon <kononenkoalexandra2002@gmail.com>
2022-08-22 23:50:55 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from './context/AuthContext';
+import Account from './pages/Account';
 import Home from "./pages/Home";
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Routes>
+    <AuthContextProvider>
+    <Navbar/>
+    <Routes>
         <Route path='/' element={<Home />}/>
-      </Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/account' element={<Account />}/>
+    </Routes>
+
+    </AuthContextProvider>
+      
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Main from '../components/Main';
 import requests from '../Request';
-import Row from './Row';
+import Row from '../components/Row';
 
 const Home = () => {
   return (
